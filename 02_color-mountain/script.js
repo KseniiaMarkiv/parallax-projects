09e242abbbf8de50c873136019d9169ba5174acc
KseniiaMarkiv <ksenya.markiv@gmail.com>
2023-01-07 22:05:13 +0200

    Added projects

---
+++ 02_color-mountain/script.js
@@ -0,0 +1,46 @@
+(function() {
+    'use strict';
+
+    gsap.from("#m1", {
+        scrollTrigger: {
+            scrub: true
+        },
+        y: 100,
+    });
+    gsap.from("#m2", {
+        scrollTrigger: {
+            scrub: true
+        },
+        y: 50,
+    });
+    gsap.from("#t1", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -70,
+    });
+    gsap.to("#t2", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -70,
+    });
+    gsap.from("#man", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -250,
+    });
+    gsap.from("#plants", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: -50,
+    });
+    gsap.from("#text", {
+        scrollTrigger: {
+            scrub: true
+        },
+        x: 500,
+    });
+})();
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # parallax-projects
-Several projects of main page with parallax 
+Several projects of main page with parallax without responsive design on tablet and less devices.
+
+[Fish link](https://kseniiamarkiv.github.io/parallax-projects/01_fish)
+![Link](https://kseniiamarkiv.github.io/parallax-projects/fish.jpg)
+
+[Mountain link](https://kseniiamarkiv.github.io/parallax-projects/02_color-mountain)
+![Link](https://kseniiamarkiv.github.io/parallax-projects/mountain.jpg)
+
+[Moon link](https://kseniiamarkiv.github.io/parallax-projects/03_moon)
+![Link](https://kseniiamarkiv.github.io/parallax-projects/moon.jpg)
+
